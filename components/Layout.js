59e037c4c3d4a9f3a50294345e7b5d9db832675b
Owali Ullah Shawon <oshawon@gmail.com>
2022-03-01 18:07:17 +0600

    navbar added with css design

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            {children}
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Layout;
