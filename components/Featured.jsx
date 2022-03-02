5a8c2f6d0b174f922a093c91534e07f291e100cb
Owali Ullah Shawon <oshawon@gmail.com>
2022-03-02 19:01:18 +0600

    order tracking  page done

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -27,7 +27,7 @@ const Featured = () => {
             <div className={styles.wrapper} style={{ transform: `translateX(${-100 * index}vw)` }}>
                 {images.map((img, i) => (
                     <div className={styles.imgContainer} key={i}>
-                        <Image src={img} alt="" layout="fill" objectFit="contain" />
+                        <Image src={img} alt="" layout="fill" />
                     </div>
                 ))}
             </div>
