b4b58d94de79871d24a7b9af118ef3913dbf5ed1
Owali Ullah Shawon <oshawon@gmail.com>
2022-03-13 11:51:50 +0600

    dynamanic total

--- components/OrderDetails.jsx
+++ components/OrderDetails.jsx
@@ -12,7 +12,7 @@ const OrderDetail = ({ total, createOrder }) => {
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <h1 className={styles.title}>You will pay $12 after delivery.</h1>
+                <h1 className={styles.title}>You will pay ${total} after delivery.</h1>
                 <div className={styles.item}>
                     <label className={styles.label}>Name Surname</label>
                     <input
