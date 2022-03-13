5c02a801c17052067365fad705dbfa2f4d7ee708
Misbah Uddin Muib <misbah15-10057@diu.edu.bd>
2022-03-13 15:32:31 +0600

    contact us form design done

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styles from "../styles/Contact.module.css";
 
 const contact = () => {
     return (
         <div>
-            Contact Page
+        <div className={styles.container}>
+            <div className={styles.contactBox}>
+                <div className={styles.left}></div>
+                <div className={styles.right}>
+                    <h2 className={styles.contactText}>Contact Us</h2>
+                    <input
+                        type="text"
+                        className={styles.field}
+                        placeholder="Your Name"
+                    />
+                    <input
+                        type="text"
+                        className={styles.field}
+                        placeholder="Your Email"
+                    />
+                    <input type="text" className={styles.field} placeholder="Phone" />
+                    <textarea
+                        placeholder="Message"
+                        className={styles.textArea}
+                    ></textarea>
+                    <button className={styles.btn}>Send</button>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
