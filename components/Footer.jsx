375cf6589e88cdb538603eced621c4d5373d0e15
Misbah Uddin Muib <misbah15-10057@diu.edu.bd>
2022-03-13 16:48:27 +0600

    footer color and content added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,48 +2,63 @@ import Image from "next/image";
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.item}>
-                <Image src="/img/bg.png" objectFit="fill" layout="fill" alt="" />
-            </div>
-            <div className={styles.item}>
-                <div className={styles.card}>
-                    <h1 className={styles.title}>TRIPOL KEBAB & GRILL</h1>
-                    <h2 className={styles.motto}>
-                        KUCHNIA LIBANSKA
-                    </h2>
-                </div>
-                {/* addresses of restaurants */}
-                <div className={styles.card}>
-                    <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
-                    <h4 className={styles.text}>
-                        Wańkowicza 3C
-                        <br /> 96-100 Skierniewice
-                        <h3>500 565 652</h3>
-                    </h4>
-                    <hr />
-                    <h4 className={styles.text}>
-                        plac Kościuszki 19a
-                        <br /> 26-340 Drzewica
-                        <h3>729 379 234</h3>
-                    </h4>
-                </div>
-                {/* time and schedule */}
-                <div className={styles.card}>
-                    <h1 className={styles.title}>WORKING HOURS</h1>
-                    <p className={styles.text}>
-                        SATURDAY TO UNTIL MONDAY
-                        <br /> 10:00 – 24:00
-                    </p>
-                    <p className={styles.text}>
-                        SUNDAY
-                        <br /> 11:00 – 24:00
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<div className={styles.item}>
+				<Image
+					src="/img/kebab-footer.jpg"
+					objectFit="fill"
+					layout="fill"
+					alt=""
+				/>
+			</div>
+			<div className={styles.item}>
+				<div className={styles.card}>
+					<h1 className={styles.title}>TRIPOL KEBAB & GRILL</h1>
+					<h2 className={styles.motto}>KUCHNIA LIBANSKA</h2>
+				</div>
+				{/* addresses of restaurants */}
+				<div className={styles.card}>
+					<h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+					<h4 className={styles.text}>
+						Wańkowicza 3C
+						<br /> 96-100 Skierniewice
+						<h3>500 565 652</h3>
+					</h4>
+					<hr />
+					<h4 className={styles.text}>
+						plac Kościuszki 19a
+						<br /> 26-340 Drzewica
+						<h3>729 379 234</h3>
+					</h4>
+				</div>
+				{/* time and schedule */}
+				<div className={styles.card}>
+					<h1 className={styles.title}>Opening Hours</h1>
+					<p className={styles.text}>
+						SATURDAY
+						<br /> 10:00 – 24:00
+					</p>
+					<p className={styles.text}>
+						SUNDAY
+						<br /> 11:00 – 24:00
+					</p>
+					<p className={styles.text}>
+						Monday
+						<br /> 11:00 – 24:00
+					</p>
+					<p className={styles.text}>
+						Wednesday
+						<br /> 11:00 – 24:00
+					</p>
+					<p className={styles.text}>
+						Thursday
+						<br /> 11:00 – 24:00
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
