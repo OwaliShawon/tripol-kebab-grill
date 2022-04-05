d1caea1556b1035f3786aea433860d5af4bde594
Owali Ullah Shawon <oshawon@gmail.com>
2022-04-05 15:52:14 +0600

    kebab card image source update

--- components/KebabCard.jsx
+++ components/KebabCard.jsx
@@ -3,19 +3,17 @@ import Link from "next/link";
 import styles from "../styles/KebabCard.module.css";
 
 const KebabCard = ({ kebab }) => {
-
-    return (
-        <div className={styles.container}>
-            <Link href={`product/${kebab._id}`} passHref>
-                <Image src="/img/menu/Philadelphia.jpg" alt="" width="500" height="500" />
-            </Link>
-            <h1 className={styles.title}>{kebab.title}</h1>
-            <span className={styles.price}>${kebab.prices[0]}</span>
-            <p className={styles.desc}>
-                {kebab.desc}
-            </p>
-        </div>
-    );
+  console.log(kebab);
+  return (
+    <div className={styles.container}>
+      <Link href={`product/${kebab._id}`} passHref>
+        <Image src={kebab.img} alt="" width="500" height="500" />
+      </Link>
+      <h1 className={styles.title}>{kebab.title}</h1>
+      <span className={styles.price}>${kebab.prices[0]}</span>
+      <p className={styles.desc}>{kebab.desc}</p>
+    </div>
+  );
 };
 
 export default KebabCard;
